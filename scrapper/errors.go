57ef462b1e53b77be3891750bc7bf2914f33cd3d
nem0z <maxime.bernard.dev@gmail.com>
2023-03-21 10:54:20 +0100

    feat (scrapper): write custom error

---
+++ scrapper/errors.go
@@ -0,0 +1,21 @@
+package scrapper
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type DoNotExistError struct {
+}
+
+type StatusError struct {
+	res *http.Response
+}
+
+func (err *DoNotExistError) Error() string {
+	return fmt.Sprintf("Page don't exist")
+}
+
+func (err *StatusError) Error() string {
+	return fmt.Sprintf("Request status error : (%v) %v", err.res.StatusCode, err.res.Status)
+}
