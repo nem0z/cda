b2361be0b311f60f493d0019b838ca8314c9eba9
nem0z <maxime.bernard.dev@gmail.com>
2023-03-22 19:39:12 +0100

    feat (main): update main

--- main.go
+++ main.go
@@ -2,57 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/nem0z/cda/scrapper"
 )
 
-func worker(indexs <-chan int, result chan *scrapper.Post) {
-	for i := range indexs {
-		res, err := scrapper.Process(i)
-		if err != nil {
-			log.Fatal(err)
-		}
-		result <- res
-	}
-}
-
 func main() {
-	start := 286049
+	start := 285086
 	n := 1000
 
-	jobs := make(chan int, n)
-	result := make(chan *scrapper.Post, n)
+	timeStart := time.Now()
 
-	s := time.Now()
+	posts := scrapper.Process(start, n)
 
-	for i := 0; i < 10; i++ {
-		go worker(jobs, result)
-	}
-
-	for i := start; i >= start-n; i-- {
-		jobs <- i
-	}
-
-	ok := 0
-	done := 0
-	go func() {
-		for {
-			time.Sleep(time.Second / 1)
-			fmt.Printf("%v/%v pages parsed out of %v\n", ok, done, n)
-		}
-	}()
-
-	for i := 0; i < n; i++ {
-		select {
-		case post := <-result:
-			done++
-			if post.Content != "" {
-				ok++
-			}
+	cpt := 0
+	for _, ok := range posts {
+		if ok != nil {
+			cpt++
 		}
 	}
 
-	fmt.Printf("%v/%v pages parsed out of %v in %v\n", ok, done, n, time.Since(s))
+	fmt.Printf("Found %v posts in %v!\n", cpt, time.Since(timeStart))
 }
