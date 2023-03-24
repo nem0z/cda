a678298d7a724b32762343d6b5caf228f9e225d1
nem0z <maxime.bernard.dev@gmail.com>
2023-03-24 15:37:23 +0100

    feat (database): init database package

---
+++ database/database.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/nem0z/cda/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Mongo struct {
+	client *mongo.Client
+	posts  *mongo.Collection
+	ctx    context.Context
+}
+
+func Init() (*Mongo, error) {
+	// init client
+	options := options.Client().ApplyURI("mongodb://localhost:27017")
+	ctx := context.TODO()
+	client, err := mongo.Connect(ctx, options)
+	if err != nil {
+		return nil, err
+	}
+
+	// check connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// load collection
+	posts := client.Database("cda").Collection("posts")
+
+	return &Mongo{
+		client,
+		posts,
+		ctx,
+	}, nil
+}
+
+func (m *Mongo) Insert(post *types.Post) (interface{}, error) {
+	fmt.Println("Inserted", post.Index)
+	result, err := m.posts.InsertOne(m.ctx, post)
+	return result.InsertedID, err
+}
+
+func (m *Mongo) Find(id interface{}) (*types.Post, error) {
+	post := &types.Post{}
+	err := m.posts.FindOne(m.ctx, bson.M{"_id": id}).Decode(&post)
+	return post, err
+}
