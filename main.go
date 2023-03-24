99c2fb09707d02cb9b0c1e56dfc0ad9949ec540e
nem0z <maxime.bernard.dev@gmail.com>
2023-03-24 15:38:00 +0100

    feat (main): insert posts in database

--- main.go
+++ main.go
@@ -2,25 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
+	"github.com/nem0z/cda/database"
 	"github.com/nem0z/cda/scrapper"
 )
 
 func main() {
-	start := 285086
-	n := 1000
+	start := 286089
+	n := 100
+
+	db, err := database.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	timeStart := time.Now()
 
+	fmt.Println("Start scrapping")
 	posts := scrapper.Process(start, n)
+	fmt.Println(posts)
 
 	cpt := 0
-	for _, ok := range posts {
-		if ok != nil {
+	for _, post := range posts {
+		if post != nil {
 			cpt++
+			db.Insert(post)
 		}
 	}
 
 	fmt.Printf("Found %v posts in %v!\n", cpt, time.Since(timeStart))
+
+	post, err := db.Find(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("First post: %+v\n", post)
 }
