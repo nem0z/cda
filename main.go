93315d920366bcf8439d3531314db014ebfd36c1
nem0z <maxime.bernard.dev@gmail.com>
2023-03-25 01:59:43 +0100

    feat (main): update main to insert new posts and fisplay the first

--- main.go
+++ main.go
@@ -2,41 +2,39 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/nem0z/cda/database"
 	"github.com/nem0z/cda/scrapper"
+	"github.com/nem0z/cda/utils"
 )
 
 func main() {
 	start := 286089
-	n := 100
+	n := 25
 
 	db, err := database.Init()
-	if err != nil {
-		log.Fatal(err)
-	}
+	utils.Handle(err)
+
+	err = db.Clear()
+	utils.Handle(err)
 
 	timeStart := time.Now()
 
-	fmt.Println("Start scrapping")
 	posts := scrapper.Process(start, n)
-	fmt.Println(posts)
 
 	cpt := 0
 	for _, post := range posts {
 		if post != nil {
 			cpt++
-			db.Insert(post)
+			db.InsertOne(post)
 		}
 	}
 
-	fmt.Printf("Found %v posts in %v!\n", cpt, time.Since(timeStart))
+	fmt.Printf("Found %v/%v posts in %v!\n", cpt, n, time.Since(timeStart))
 
-	post, err := db.Find(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	post, err := db.FindAll()
+	utils.Handle(err)
+
 	fmt.Printf("First post: %+v\n", post)
 }
